ee5d0f08d09fc23fd4a834e9a2bfc302f0857744
Max <jmksm96@gmail.com>
2021-11-24 16:01:17 +0300

    Layout improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { Container, Main } from 'ng-particles';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-swapi';
+  id = 'tsparticles';
+
+  particlesOptions = {
+    particles: {
+      color: {
+        value: ['#ffff'],
+      },
+      move: {
+        enable: true,
+        speed: 1,
+      },
+      background: {
+        position: '50% 50%',
+        repeat: 'no-repeat',
+        size: '20%',
+        opacity: 1,
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false,
+        },
+      },
+    },
+  };
+
+  particlesLoaded(container: Container): void {
+    console.log(container);
+  }
+
+  particlesInit(main: Main): void {
+    console.log(main);
+
+    // Starting from 1.19.0 you can add custom presets or shape here, using the current tsParticles instance (main)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgParticlesModule } from 'ng-particles';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    NgParticlesModule,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
