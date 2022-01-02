68de95a1ee8fc93e133f57d1dc107ec85083f54b
unknown <maxim.yarosh@section228.com>
2022-01-02 11:41:25 +0300

    characters done

--- src/app/features/components/characters/pages/characters-item/character-item.component.ts
+++ src/app/features/components/characters/pages/characters-item/character-item.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { finalize } from 'rxjs/operators';
+import { finalize, map } from 'rxjs/operators';
 import { setID } from 'src/app/core/helpers/set-id';
 import { Character } from 'src/app/core/models/character';
-import { FilmResponse } from 'src/app/core/models/film';
+import { Film } from 'src/app/core/models/film';
+import { Starship } from 'src/app/core/models/starship';
+import { Vehicle } from 'src/app/core/models/vehicle';
 import { CharactersService } from 'src/app/core/services/characters.service';
 import { FilmsService } from 'src/app/core/services/films.service';
 import { ImageService } from 'src/app/core/services/image.service';
+import { StarshipsService } from 'src/app/core/services/starships.service';
+import { VehiclesService } from 'src/app/core/services/vehicles.service';
 
 @Component({
   selector: 'app-character',
@@ -19,15 +23,18 @@ export class CharactersItemComponent implements OnInit {
   isLoading!: boolean;
   homeworldId!: number;
   vehiclesId!: number[];
-  films?: FilmResponse[] = [];
   filmsId!: number[];
   starshipsId!: number[];
-  message!: string;
+  films?: Film[] = [];
+  starships?: Starship[] = [];
+  vehicles?: Vehicle[] = [];
   img!: string | ArrayBuffer | null;
 
   constructor(
     private charactersService: CharactersService,
     private filmsService: FilmsService,
+    private starshipsService: StarshipsService,
+    private vehiclesService: VehiclesService,
     private activateRoute: ActivatedRoute,
     private imageService: ImageService
   ) {
@@ -37,42 +44,55 @@ export class CharactersItemComponent implements OnInit {
   ngOnInit(): void {
     this.activateRoute.params.subscribe((res) => {
       this.getCharacterInfo(res.id);
-      this.imageService.getImg(res.id, 'characters').subscribe(
-        (res: any) => {
-          this.createImageFromBlob(res);
-        },
-        (error) => {
-          this.imageService
-            .getPlaceholder()
-            .subscribe((res) => this.createImageFromBlob(res));
-        }
-      );
+      this.getImg(+res.id, 'characters');
     });
+  }
+
+  getImg(id: number, category: string) {
+    this.imageService.getImg(id, category).subscribe(
+      (res: Blob) => {
+        this.createImageFromBlob(res);
+      },
+      (error) => {
+        this.imageService
+          .getPlaceholder()
+          .subscribe((res) => this.createImageFromBlob(res));
+      }
+    );
   }
 
   getElements(externalID?: number[]) {
     switch (externalID) {
       case this.filmsId:
         externalID.forEach((id) => {
-          this.filmsService.getFilm(id).subscribe((res) => {
-            this.films!.push(res);
-          });
+          this.filmsService
+            .getFilm(id)
+            .pipe(map((i) => ({ ...i, id: id })))
+            .subscribe((res) => {
+              this.films!.push(new Film(res));
+            });
         });
         break;
 
-      case this.filmsId:
+      case this.vehiclesId:
         externalID.forEach((id) => {
-          this.filmsService.getFilm(id).subscribe((res) => {
-            this.films!.push(res);
-          });
+          this.vehiclesService
+            .getVehicle(id)
+            .pipe(map((i) => ({ ...i, id: id })))
+            .subscribe((res) => {
+              this.vehicles!.push(new Vehicle(res));
+            });
         });
         break;
 
-      case this.filmsId:
+      case this.starshipsId:
         externalID.forEach((id) => {
-          this.filmsService.getFilm(id).subscribe((res) => {
-            this.films!.push(res);
-          });
+          this.starshipsService
+            .getStarship(id)
+            .pipe(map((i) => ({ ...i, id: id })))
+            .subscribe((res) => {
+              this.starships!.push(new Starship(res));
+            });
         });
         break;
 
@@ -100,18 +120,6 @@ export class CharactersItemComponent implements OnInit {
     this.filmsId = this.data.films.map((res) => setID(res));
     this.vehiclesId = this.data.vehicles.map((res) => setID(res));
     this.starshipsId = this.data.starships.map((res) => setID(res));
-
-    // console.log(this.films);
-
-    this.contentChecker(this.filmsId);
-    this.contentChecker(this.vehiclesId);
-    this.contentChecker(this.starshipsId);
-  }
-
-  contentChecker(id: number[]) {
-    if (id.length <= 0) {
-      this.message = 'No related content';
-    }
   }
 
   createImageFromBlob(image: Blob) {
